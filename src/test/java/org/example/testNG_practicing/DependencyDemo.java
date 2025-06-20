f33ce078f9402c315363c648d47fa8b4b7735e83
jihad <jihad.owais@t3.com.eg>
2025-06-20 23:46:14 +0300

    Add DependencyDemo class & apply dependsOnMethods parameter

---
+++ src/test/java/org/example/testNG_practicing/DependencyDemo.java
@@ -0,0 +1,33 @@
+package org.example.testNG_practicing;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class DependencyDemo {
+    @BeforeClass
+    public void setUp(){ System.out.println("PriorityDemo Class -> @BeforeClass Annotation. setUp() method."); }
+
+    @AfterClass
+    public void clean(){ System.out.println("PriorityDemo Class -> @AfterClass Annotation. clean() method."); }
+
+    @Test(priority = 1, dependsOnMethods = {"testMethodC", "testMethodD"})
+    public void testMethodB(){ System.out.println("PriorityDemo Class -> @Test Annotation. testMethodB() test method."); }
+
+    @Test(priority = 2)
+    public void testMethodA(){
+        System.out.println("PriorityDemo Class -> @Test Annotation. testMethodA() test method.");
+    }
+
+    @Test(priority = 4)
+    public void testMethodC(){
+        System.out.println("PriorityDemo Class -> @Test Annotation. testMethodC() test method.");
+        Assert.fail();
+    }
+
+    @Test(priority = 3)
+    public void testMethodD(){
+        System.out.println("PriorityDemo Class -> @Test Annotation. testMethodD() test method.");
+    }
+}
