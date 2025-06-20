c2fa30d63243b5799e0a53e94517f24553742fab
jihad <jihad.owais@t3.com.eg>
2025-06-20 23:19:39 +0300

    Add PriorityDemo class & apply priority parameter

---
+++ src/test/java/org/example/testNG_practicing/PriorityDemo.java
@@ -0,0 +1,26 @@
+package org.example.testNG_practicing;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class PriorityDemo {
+    @BeforeClass
+    public void setUp(){ System.out.println("PriorityDemo Class -> @BeforeClass Annotation. setUp() method."); }
+
+    @AfterClass
+    public void clean(){ System.out.println("PriorityDemo Class -> @AfterClass Annotation. clean() method."); }
+
+    @Test(priority = 0)
+    public void testMethodB(){ System.out.println("PriorityDemo Class -> @Test Annotation. testMethodB() test method."); }
+
+    @Test(priority = 1)
+    public void testMethodA(){
+        System.out.println("PriorityDemo Class -> @Test Annotation. testMethodA() test method.");
+        Assert.assertTrue(false);
+    }
+
+    @Test(priority = 2)
+    public void testMethodC(){ System.out.println("PriorityDemo Class -> @Test Annotation. testMethodC() test method."); }
+}
