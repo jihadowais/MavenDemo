b4fcd9b35dcdc929a5e979b5765d27f0efd77fd5
jihad <jihad.owais@t3.com.eg>
2025-06-22 04:52:40 +0300

    Apply SoftAssert assertAll() technique

--- src/test/java/org/example/testNG_practicing/Assertions/AssertionTest.java
+++ src/test/java/org/example/testNG_practicing/Assertions/AssertionTest.java
@@ -2,6 +2,7 @@ package org.example.testNG_practicing.Assertions;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class AssertionTest {
 
@@ -14,6 +15,26 @@ public class AssertionTest {
 
         int actualSum = ClassUnderTest.sum(testDataFirstNumber, testDataSecondNumber);
         Assert.assertEquals(actualSum, expectedSum);
+    }
+
+    @Test
+    public void testSumAll(){
+        System.out.println("Running Test -> testSum...");
+        int expectedSum1 = 3;
+        int testDataFirstNumber = 1;
+        int testDataSecondNumber = 2;
+
+        int expectedSum2 = 8;
+        int testDataThirdNumber = 8;
+        int testDataFourthNumber = 5;
+
+        int actualSum1 = ClassUnderTest.sum(testDataFirstNumber, testDataSecondNumber);
+        int actualSum2 = ClassUnderTest.sum(testDataThirdNumber, testDataFourthNumber);
+
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(actualSum1, expectedSum1);
+        softAssert.assertEquals(actualSum2, expectedSum2);
+        softAssert.assertAll();
     }
 
     @Test
